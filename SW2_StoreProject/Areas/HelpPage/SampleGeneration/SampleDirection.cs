4c539ba6a76c033b0fbdc539f823abbec0e4f358
Z,Taha <ziaddtahaa@gmail.com>
2020-04-02 16:39:29 +0200

    merge with master

---
+++ SW2_StoreProject/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace SW2_StoreProject.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
